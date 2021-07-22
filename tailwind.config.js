4c26d0f06269e8c052b4b322798de741e883bdbd
Justin Byrne <theskaterjesus@gmail.com>
2021-07-22 20:10:28 +0100

    Created extra colours and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppin: 'Poppins, sans-serif',
+      },
+      colors: {
+        cyan: 'hsl(180, 62%, 55%)',
+        orange: 'hsl(34, 97%, 64%)',
+        blue: 'hsl(212, 86%, 64%)',
+        red: 'hsl(0, 78%, 62%)',
+        darkBlue: 'hsl(234, 12%, 34%)',
+        grayBlue: 'hsl(229, 6%, 66%)',
+        lightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
   },
   variants: {
     extend: {},
